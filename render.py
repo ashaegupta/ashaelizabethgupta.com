70ff0fa1ab7e3118ba5748cae40aaaf7100de3c7
Shreyans Bhansali <shreyans@venmo.com>
2012-03-24 12:24:53 -0400

    added a ux section to the projects page

--- render.py
+++ render.py
@@ -4,9 +4,10 @@ import codecs
 from jinja2 import Environment, FileSystemLoader
 from project_data import project_data, ux_data # json data for projects
 
-combined_project_data = dict(project_data.data.items() + ux_data.data.items()),
+combined_project_data = dict(project_data.data.items() + ux_data.data.items())
 
 templates_dir = 'templates'
+env = Environment(loader = FileSystemLoader(templates_dir))
 
 templates_to_render = {
     'index':{'index_page':1},
@@ -14,12 +15,10 @@ templates_to_render = {
     'pictures':{'pictures_page':1}
 }
 
-env = Environment(loader = FileSystemLoader(templates_dir))
-
-for template_root in templates_to_render:
+for template_root, template_data in templates_to_render.iteritems():
     template_name = "%s_template.html" % template_root
     template = env.get_template(template_name)
-    html = template.render(templates_to_render[template_root])
+    html = template.render(template_data)
     file_name = "%s.html" % template_root
     f = codecs.open(file_name, encoding='utf-8', mode='w')
     f.write(html)
