1239b6109b5c0d879420018aced8befaeacbcb27
Shreyans Bhansali <shreyans@venmo.com>
2012-03-25 13:19:56 -0400

    ux_images.json created

--- project_data/create_project_data_json.py
+++ project_data/create_project_data_json.py
@@ -1,12 +1,17 @@
+import os
+import simplejson
 import csv
 import pprint
 from collections import defaultdict
 
 project_csv_file = 'inputs/projects.csv'
 ux_csv_file = 'inputs/ux.csv'
+ux_images_directory = '../static/images/ux'
+ux_image_directory_for_static = 'static/images/ux/'
 
 projects_data_py_file = 'outputs/project_data.py'
 ux_data_py_file = 'outputs/ux_data.py'
+ux_images_py_file = '../static/json/ux_images.json'
 
 def utf_8_encoder(unicode_csv_data):
     for line in unicode_csv_data:
@@ -54,7 +59,7 @@ def write_project_data():
     data[u'personal_mantra'] = unicode("'You are already naked. There is no reason not to follow your heart.' -- Steve Jobs")
     data[u'ux_mantra'] = unicode("I like pretty things")
 
-    print "saving file..."
+    print "saving file %s..." % projects_data_py_file
     data_str = "data = %s" % dict(data)
     print data_str
     projects_data_file = open(projects_data_py_file, mode='w')
@@ -88,7 +93,7 @@ def write_ux_data():
         # and save the dictionary in the array of all ux data
         data['ux'].append(item)
 
-    print "saving file..."
+    print "saving file %s..." % ux_data_py_file
     data_str = "data = %s" % dict(data)
     print pprint.pformat(dict(data))
     ux_data_file = open(ux_data_py_file, mode='w')
@@ -97,10 +102,28 @@ def write_ux_data():
 
 def write_ux_image_list_data():
     print "checking contents of /static/images/ux/"
+    image_directories = os.listdir(ux_images_directory)
+    image_directories_to_ignore = ['drafts']
+    image_data = dict()
+    for dir in image_directories:
+        if dir in image_directories_to_ignore:
+            continue
+        files = os.listdir(os.path.join(ux_images_directory, dir))
+        image_data[dir] = [os.path.join(ux_image_directory_for_static,
+                                        dir, 
+                                        file) for file in files]
+
+    print "saving file %s..." % ux_images_py_file
+    data_json = simplejson.dumps(image_data)
+    ux_data_file = open(ux_images_py_file, mode='w')
+    ux_data_file.write(data_json)
+    ux_data_file.close()
+        
 
 
 if __name__ == "__main__":
     print "running..."
     write_project_data()
     write_ux_data()
+    write_ux_image_list_data()
     print "done."
